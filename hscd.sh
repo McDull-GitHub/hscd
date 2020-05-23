b1ce2ab43ae5720cdbae17f6e7525aea888f5b85
Mcdull <1042762344@qq.com>
2020-05-23 17:05:41 +0800

    Add files via upload

---
+++ hscd.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+link="http://www.tvboxnow.com/thread-4881427-1-1.html"
+EP=600
+do
+    curl $link | grep torrent | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g'
+    link2download=$(curl $link | grep torrent | grep H265 | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g')
+    download_torrent=$(curl -L "http://www.tvboxnow.com/$link2download" | grep 如果 | sed 's/.*href=\"\(at.*\)\"/\1/g' | sed 's/>.*//g')
+    curl -o $EP.torrent -L http://www.tvboxnow.com/$download_torrent
+    aria2c --seed-time=0 *.torrent
+    rm *.aria2
+    cd TVBOX*
+    IFS=$'\n'
+    file=`ls *.mp4`
+    upfile=`echo $file | sed 's/TVBOXNOW\ //g'` 
+    mv $file ../$upfile
+    cd .. 
+    pip3 install telethon --user
+    echo $upfile
+    python3 u2tggroup.py ./$upfile
+    rm -rf TVBOXNOW* *.torrent *.mp4
+    let EP=EP+1
+done
---
+++ u2tggroup.py
@@ -0,0 +1,11 @@
+from telethon.sync import TelegramClient
+import sys
+api_id = 1054981
+api_hash = '341e29114e1bb38d1fda9f1a22b59b28'
+file = sys.argv[1]
+print(type(file))
+print(file)
+with TelegramClient('group', api_id, api_hash) as client:
+   def callback(current, total):
+      print('Uploaded', current, 'out of', total,'bytes: {:.2%}'.format(current / total))
+   client.send_file('me',file,force_document=True,progress_callback=callback)
