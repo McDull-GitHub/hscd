05af1974282f779fd809ac1966d4fe23e78c59ae
Mcdull <1042762344@qq.com>
2020-05-23 17:36:41 +0800

    Update hscd.sh

--- hscd.sh
+++ hscd.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 link="http://www.tvboxnow.com/thread-4881427-1-1.html"
-EP=700
+EP=800
 while :;
 do
     curl $link | grep torrent | sed 's/.*href=\"\(at.*\)\"/\1/g' | grep EP$EP | sed 's/\".*//g'
